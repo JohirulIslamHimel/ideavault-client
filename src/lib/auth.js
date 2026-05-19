72ce4f39adffac244e790322a444e56112b691db
Johirul Islam Himel <himel1199@gmail.com>
2026-05-20 02:38:47 +0600

    google credentials connected successfully

---
+++ src/lib/auth.js
@@ -0,0 +1,14 @@
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+  },
+});
